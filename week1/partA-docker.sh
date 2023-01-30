19dfe863cc68acb5f2fc0e3900c025534e28b48b
Wonhyeong Seo <wonhseo@kakao.com>
2023-01-30 23:40:52 +0900

    partA complete

---
+++ week1/partA-docker.sh
@@ -0,0 +1,10 @@
+# /bin/sh
+# Q1.
+docker --help | grep image # --iidfile string
+# Q2.
+docker run -it --rm --entrypoint /bin/sh python:3.9
+pip list # 3: pip, setuptools, wheels
+# Q3. 20530
+# Q4. 2019-01-15
+# Q5. 2: 1282; 3: 254
+# Q6. Long Island City/Queens Plaza
